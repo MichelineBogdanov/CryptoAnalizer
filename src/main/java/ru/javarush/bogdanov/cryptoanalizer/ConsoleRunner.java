efb64fe99e2cdf17d4a65e81ba08bb43d0304728
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2022-03-17 21:26:44 +0300

    test commit 2 (check branch)

---
+++ src/main/java/ru/javarush/bogdanov/cryptoanalizer/ConsoleRunner.java
@@ -0,0 +1,7 @@
+package ru.javarush.bogdanov.cryptoanalizer;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        System.out.println("ghbdtn");
+    }
+}
--- src/main/java/ru/javarush/bogdanov/cryptoanalizer/Runner.java
+++
@@ -1,4 +0,0 @@
-package ru.javarush.bogdanov.cryptoanalizer;
-
-public class Runner {
-}
