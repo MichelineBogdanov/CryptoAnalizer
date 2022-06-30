f66fa992a38737b3408557235f3b75fd19e7bb27
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2022-06-30 23:04:19 +0300

    - обновлен ReadMe

--- src/main/java/ru/javarush/bogdanov/cryptoanalizer/exeptions/ValidateException.java
+++ src/main/java/ru/javarush/bogdanov/cryptoanalizer/exeptions/ValidateException.java
@@ -3,6 +3,5 @@ package ru.javarush.bogdanov.cryptoanalizer.exeptions;
 public class ValidateException extends RuntimeException{
     public ValidateException(String message) {
         super(message);
-
     }
 }
